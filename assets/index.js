132fbbdedf0708026ddcde345e00d78f4d741d90
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-01-19 21:43:24 -0500

    Added JS to insert date into HTML

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,13 @@
 //Psuedo coding
 //JavaScript to do
-//When planner is open it displays current day at top of planner
+
+//Creates variable for current date & time in my preferred format, sorry Brits
+//Creates variable to select HTML element to insert into
+let date = moment().format('MMMM Do YYYY, h:mm:ss a');
+let insertDate = document.getElementById('currentDay');
+
+//Inserts current date into HTML
+insertDate.innerHTML = date;
 
 //Each time block is color-coded to indicate whether it is in the past, present, or future (already in CSS)
 
