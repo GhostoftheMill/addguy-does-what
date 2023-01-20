42110246cc22b87cec3c3d697dc2c0836adf03aa
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-01-19 20:06:33 -0500

    Added to do list for JS

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+//Psuedo coding
+//JavaScript to do
+//When planner is open it displays current day at top of planner
+
+//Each time block is color-coded to indicate whether it is in the past, present, or future (already in CSS)
+
+//When I click on a time block, I can enter an event
+
+//When I click the save button the text is saved in local storage
+
+//When I refresh the page the saved events persist
+
